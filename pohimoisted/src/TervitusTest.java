69154f026d9f7073b9400763dd3564999fe58aaa
eva-liisa.edela <eva-liisa.edela@khk.ee>
2018-11-15 10:03:08 +0200

    Lisatud teine tervitus vaikneTervitus kui teine väärtus meie funktsioonile

--- pohimoisted/src/TervitusTest.java
+++ pohimoisted/src/TervitusTest.java
@@ -1,6 +1,10 @@
 public class TervitusTest {
     public static void main(String[] args) {
         Tervitus evaTervitus = new Tervitus ("Tere Õpilased!");
+        Tervitus vaikneTervitus = new Tervitus ();
         evaTervitus.tervita();
+        vaikneTervitus.tervita();
     }
+
+
 }
